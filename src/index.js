b9deb8c6b8512da6e34e5f0ff0ed99055fdc9ae6
Luiz dos Reis <luizdosreis@gmail.com>
2018-11-22 21:07:42 -0200

    created styled component for css reset

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'react-dates/initialize';
-import moment from 'moment';
 import numeral from 'numeral';
 import App from './App';
 
 import configureStore from './store/configureStore';
-import { addCategory } from './actions/categories';
-import { add } from './actions/expenses';
 
-import 'normalize.css/normalize.css';
 import 'purecss/build/pure.css';
 import './styles/side-menu.css';
 import './styles/styles.css';
 import 'react-dates/lib/css/_datepicker.css';
+import Reset from './styles/generic/Reset';
 
 const store = configureStore();
 
@@ -39,21 +36,12 @@ numeral.register('locale', 'pt-BR', {
 
 numeral.locale('pt-BR');
 
-store.dispatch(addCategory({ description: 'Car', type: 'OUT' }));
-store.dispatch(addCategory({ description: 'Motorcycle', type: 'OUT' }));
-store.dispatch(addCategory({ description: 'Salary', type: 'IN' }));
-store.dispatch(
-  add({
-    description: 'Gas',
-    category: 'Car',
-    createAt: moment(),
-    amount: 1000.234,
-  }),
-);
-
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Fragment>
+      <Reset />
+      <App />
+    </Fragment>
   </Provider>,
   document.getElementById('root'),
 );
